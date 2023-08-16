592afefa91fdd5de0ee5edf46b71b9fe1ae357f9
Kie <kito0524s@gmail.com>
2023-08-16 15:37:26 +0900

    プログラムファイルをライブラリごとに分割

--- NimQuizApplication/src/Graphic.nim
+++ NimQuizApplication/src/Graphic.nim
@@ -1,2 +1,12 @@
+#ライブラリのインポート
+import wNim
+
 proc Start*() =
   echo("Graphic")
+
+  let app = App()
+  let frame = Frame(title="Hello World", size=(1600, 900))
+
+  frame.center()
+  frame.show()
+  app.mainLoop()
--- NimQuizApplication/src/NimQuizApplication.nim
+++ NimQuizApplication/src/NimQuizApplication.nim
@@ -1,34 +1,10 @@
-#ライブラリのインポート
-import wNim, slappy, db_connector/db_sqlite
-
 #プログラムファイルのインポート
-import Initialize, DB_Connection, Graphic
+import Initialize, DB_Connection, Graphic, Sound
 
 when isMainModule:
   Initialize.Start()
   DB_Connection.Start()
+  Sound.Start()
   Graphic.Start()
 
   echo("Hello, World!")
-
-  let db = open("Data/QUIZ_DATABASE.db", "", "", "")
-
-  db.exec(sql"""CREATE TABLE IF NOT EXISTS QUIZ_TABLE (
-    id    INTEGER PRIMARY KEY,
-    name  VARCHAR(50) NOT NULL,
-    i     INT(11),
-    f     DECIMAL(18, 10)
-  )""")
-
-  db.close()
-
-  slappyInit()
-
-  slappyClose()
-
-  let app = App()
-  let frame = Frame(title="Hello World", size=(1600, 900))
-
-  frame.center()
-  frame.show()
-  app.mainLoop()
