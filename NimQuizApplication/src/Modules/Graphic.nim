3581a7f79ef73ea080951cfc92499e7b918893aa
Kie <kito0524s@gmail.com>
2023-12-17 19:14:47 +0900

    どうでしょう

--- NimQuizApplication/src/Modules/Graphic.nim
+++ NimQuizApplication/src/Modules/Graphic.nim
@@ -2,6 +2,7 @@
 #ライブラリのインポート
 #################################################
 import wNim
+import screen_layout
 
 
 #################################################
@@ -11,8 +12,22 @@ var
   app: wApp
   frame: wFrame
   panel: wPanel
-  listbox: wListBox
-  label: wTextCtrl
+  #listbox: wListBox
+  #label: wTextCtrl
+
+  title: wStaticText
+  info: wStaticText
+  genre: wStaticText
+  question: wTextCtrl
+  option1: wButton
+  option2: wButton
+  #options3: wButton
+  #options4: wButton
+  prev: wButton
+  next: wButton
+
+  selected_option: uint8
+  correct_answer: uint8
 
 
 #################################################
@@ -20,7 +35,7 @@ var
 #################################################
 proc event()
 proc layout()
-proc add(self: wListBox, text: string)
+#proc add(self: wListBox, text: string)
 
 
 #################################################
@@ -32,19 +47,28 @@ proc assignment*() =
   app = App(wSystemDpiAware)
   frame = Frame(title=Title, size=(1280, 720))
   panel = Panel(frame)
-  listbox = ListBox(panel, style=wBorderSimple or wLbNeededScroll)
-  label = TextCtrl(panel, value="Label", style=wTeReadOnly)
+  #listbox = ListBox(panel, style=wBorderSimple or wLbNeededScroll)
+  #label = TextCtrl(panel, value="Label", style=wTeReadOnly)
 
+  title = StaticText(panel, label="スタジオララの名前", style=(wAlignCenter + wAlignMiddle))
+  info = StaticText(panel, label="1/1", style=(wAlignCenter + wAlignMiddle))
+  genre = StaticText(panel, label="〇×ゲーム", style=(wAlignCenter + wAlignMiddle))
+  question = TextCtrl(panel, value="スタジオララ(旧:田中工務店)の正式名称は「Sutudio RaLa」である。\n〇か×か？", style=wTeReadOnly)
+  option1 = Button(panel, label="〇")
+  option2 = Button(panel, label="×")
+  #options3 = Button(panel, label="options3")
+  #options4 = Button(panel, label="options4")
+  prev = Button(panel, label="prev")
+  next = Button(panel, label="next")
 
-  setFont(listbox, Font(20))
-
+  correct_answer = 2
 
 #################################################
 #   メインプロシージャ
 #################################################
 proc main*() =
-  for i in 0..10:
-    listbox.add "Button" & $i
+#  for i in 0..10:
+#    listbox.add "Button" & $i
 
   layout()
   event()
@@ -67,22 +91,55 @@ proc window_close*() =
 #   通常処理
 #################################################
 proc layout() =
-  panel.autolayout "spacing: 8 \nH:|-[label(40%)]-[listbox]-| \nV:|-[label,listbox]-|"
+  var font_size = (frame.getsize.width + frame.getsize.height)/200
 
-proc add(self: wListBox, text: string) =
-  self.ensureVisible(self.append(text))
+  panel.autolayout(screen_layout.get_string("two_choice"))
+
+  setFont(title, Font(font_size))
+  setFont(info, Font(font_size))
+  setFont(genre, Font(font_size))
+  setFont(question, Font(font_size))
+  setFont(option1, Font(font_size))
+  setFont(option2, Font(font_size))
+  setFont(prev, Font(font_size))
+  setFont(next, Font(font_size))
+
+#proc add(self: wListBox, text: string) =
+#  self.ensureVisible(self.append(text))
 
 
 #################################################
 #   イベント処理
 #################################################
 proc event() =
-  listbox.wEvent_ListBox do ():
-    label.setValue($(listbox.getSelection()))
-    echo $(listbox.getSelection())
+#  listbox.wEvent_ListBox do ():
+#    label.setValue($(listbox.getSelection()))
+#    echo $(listbox.getSelection())
 
   panel.wEvent_Size do ():
     layout()
 
-  frame.wIdClear do ():
-    listbox.clear()
+#  frame.wIdClear do ():
+#    listbox.clear()
+
+  option1.wEvent_Button do ():
+    selected_option = 1
+    echo("Selected Button No." & $selected_option)
+
+  option2.wEvent_Button do ():
+    selected_option = 2
+    echo("Selected Button No." & $selected_option)
+
+  prev.wEvent_Button do ():
+    echo("prev")
+  
+  next.wEvent_Button do ():
+    echo("next")
+
+    if correct_answer == selected_option:
+      echo("correct")
+      setValue(question, value="正解！")
+
+    else:
+      echo("incorrect")
+      setValue(question, value="不正解！")
