03224b305a7fa83aefcb4789eb12d7bc6ef7ba85
Kie <kito0524s@gmail.com>
2023-12-17 21:11:55 +0900

    要らない部分を削除

--- NimQuizApplication/src/Modules/Graphic.nim
+++ NimQuizApplication/src/Modules/Graphic.nim
@@ -12,8 +12,6 @@ var
   app: wApp
   frame: wFrame
   panel: wPanel
-  #listbox: wListBox
-  #label: wTextCtrl
 
   title: wStaticText
   info: wStaticText
@@ -35,7 +33,6 @@ var
 #################################################
 proc event()
 proc layout()
-#proc add(self: wListBox, text: string)
 
 
 #################################################
@@ -47,13 +44,11 @@ proc assignment*() =
   app = App(wSystemDpiAware)
   frame = Frame(title=Title, size=(1280, 720))
   panel = Panel(frame)
-  #listbox = ListBox(panel, style=wBorderSimple or wLbNeededScroll)
-  #label = TextCtrl(panel, value="Label", style=wTeReadOnly)
 
   title = StaticText(panel, label="スタジオララの名前", style=(wAlignCenter + wAlignMiddle))
   info = StaticText(panel, label="1/1", style=(wAlignCenter + wAlignMiddle))
   genre = StaticText(panel, label="〇×ゲーム", style=(wAlignCenter + wAlignMiddle))
-  question = TextCtrl(panel, value="スタジオララ(旧:田中工務店)の正式名称は「Sutudio RaLa」である。\n〇か×か？", style=wTeReadOnly)
+  question = TextCtrl(panel, value="スタジオララ(旧:田中工務店)の正式名称は「Sutudio RaLa」である。〇か×か？", style=(wTeReadOnly + wTeMultiLine))
   option1 = Button(panel, label="〇")
   option2 = Button(panel, label="×")
   #options3 = Button(panel, label="options3")
@@ -67,9 +62,6 @@ proc assignment*() =
 #   メインプロシージャ
 #################################################
 proc main*() =
-#  for i in 0..10:
-#    listbox.add "Button" & $i
-
   layout()
   event()
   frame.center()
@@ -104,23 +96,13 @@ proc layout() =
   setFont(prev, Font(font_size))
   setFont(next, Font(font_size))
 
-#proc add(self: wListBox, text: string) =
-#  self.ensureVisible(self.append(text))
-
 
 #################################################
 #   イベント処理
 #################################################
 proc event() =
-#  listbox.wEvent_ListBox do ():
-#    label.setValue($(listbox.getSelection()))
-#    echo $(listbox.getSelection())
-
   panel.wEvent_Size do ():
     layout()
-
-#  frame.wIdClear do ():
-#    listbox.clear()
 
   option1.wEvent_Button do ():
     selected_option = 1
