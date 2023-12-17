ac2d7135ed32c42d45d4ecd4c083b8c5eed8a701
Kie <kito0524s@gmail.com>
2023-12-18 00:06:13 +0900

    ４択クイズ用レイアウト追加

--- NimQuizApplication/src/Modules/screen_layout.nim
+++ NimQuizApplication/src/Modules/screen_layout.nim
@@ -18,6 +18,24 @@ proc get_string*(layout_name: string): string =
 
         H:[Fooder:[prev(20%)]-[genre]-[next(20%)]]"""
 
+  of "four_choice":
+    layout_string = "spacing: " & $space & """
+        H:|-(10%)-[Header]-(10%)-|
+        H:|-(20%)-[Question]-(20%)-|
+        H:|-(10%)-[Options]-(10%)-|
+        H:|-(10%)-[Fooder]-(10%)-|
+        V:|-[Header(10%)]-[Question(40%)]-[Options]-[Fooder(10%)]-|
+
+        H:[Header:-[title(80%)]-[info]-]
+
+        H:[Question:[question]]
+
+        H:[Options:[OptionsL(OptionsR)]-[OptionsR]]
+        V:[OptionsL:[option1(option3)]-[option3]]
+        V:[OptionsR:[option2(option4)]-[option4]]
+
+        H:[Fooder:[prev(20%)]-[genre]-[next(20%)]]"""
+
   else:
     layout_string = """
         spacing: 8
