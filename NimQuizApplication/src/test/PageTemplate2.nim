77ca57486aad0bc710cc6ed66b6e4ad1409dce80
Kie <kito0524s@gmail.com>
2024-01-02 23:57:16 +0900

    別モジュール上でイベントを拾えることは確認できましたが、同様のイベントハンドラーを2回読み込んだ場合、後に読み込まれたものが優先されることを確認しました・・・

--- NimQuizApplication/src/test/PageTemplate2.nim
+++ NimQuizApplication/src/test/PageTemplate2.nim
@@ -2,44 +2,44 @@ import wNim
 import ../Modules/public_variables
 import main
 
-const my_state*: MenuState = stSetting
-const prev_state*: MenuState = stCredit
-const next_state*: MenuState = stCredit
+const my_state*: MenuState = stCredit
+const prev_state*: MenuState = stSetting
+const next_state*: MenuState = stSetting
 
-const layout_string: string = "spacing:" & $layout_spacing & """
-        H:|-[conTitle,BOTTOM]-|
-        V:|-[conTitle]-[BOTTOM(20%)]-|
+const layout_string2: string = "spacing:" & $layout_spacing & """
+        H:|-[conTitle2,BOTTOM]-|
+        V:|-[conTitle2]-[BOTTOM(20%)]-|
         
         H:[BOTTOM:[conPrev(conNext,30%)]~[conNext]]"""
 
 proc layout*(state: MenuState)
 
 proc init*() =
-  echo("PageTemplate")
+  echo("PageTemplate2")
 
-  echo(getTitle(conTitle))
+  echo(getTitle(conTitle2))
 
 proc layout*(state: MenuState) =
-  echo("layout1")
+  echo("layout2")
 
   if my_state != now_state:
     echo("component_refresh")
     now_state = my_state
 
   
-  main_panel.autolayout(layout_string)
+  main_panel.autolayout(layout_string2)
 
 proc event*() =
-  echo("event1")
+  echo("event2")
 
   main_panel.wEvent_Size do ():
-    echo("size1")
+    echo("size2")
     layout(my_state)
 
   conPrev.wEvent_Button do ():
-    echo("prev1")
+    echo("prev2")
     callMenu(prev_state)
 
   conNext.wEvent_Button do ():
-    echo("next1")
+    echo("next2")
     callMenu(next_state)
--- NimQuizApplication/src/test/main.nim
+++ NimQuizApplication/src/test/main.nim
@@ -2,20 +2,46 @@ import wNim
 import ../Modules/public_variables
 
 var
-  app: wApp = App(wSystemDpiAware)
-  frame*: wFrame = Frame(title="テスト", size=(1280, 720))
-  panel*: wPanel = Panel(frame, style=wDoubleBuffered)
-  title*: wStaticText = StaticText(panel, label="TITLE", style=(wAlignCenter + wAlignMiddle))
+  now_state*: MenuState = stDefault
 
-export frame, panel, title
+  main_app: wApp = App(wSystemDpiAware)
+  main_frame*: wFrame = Frame(title="テスト", size=(1280, 720))
+  main_panel*: wPanel = Panel(main_frame, style=wDoubleBuffered)
+  conTitle*: wStaticText = StaticText(main_panel, label="TITLE", style=(wAlignCenter + wAlignMiddle))
+  conTitle2*: wStaticText = StaticText(main_panel, label="TITLE2", style=(wAlignCenter + wAlignMiddle))
+  conPrev = Button(main_panel, label="prev")
+  conNext = Button(main_panel, label="next")
 
-import PageTemplate
+proc initMenu()
+proc callMenu*(state: MenuState)
+
+export main_frame, main_panel, conTitle, conPrev, conNext
+
+import PageTemplate, PageTemplate2
 
 when isMainModule:
-  PageTemplate.layout()
+  initMenu()
+
+  callMenu(stSetting)
+
+  main_frame.center()
+  main_frame.show()
+  main_app.mainLoop()
+
+proc initMenu() =
+  PageTemplate.init()
+  PageTemplate2.init()
+
   PageTemplate.event()
+  PageTemplate2.event()
 
+proc callMenu*(state: MenuState) =
+  case state
+  of stSetting:
+    PageTemplate.layout(state)
 
-  frame.center()
-  frame.show()
-  app.mainLoop()
+  of stCredit:
+    PageTemplate2.layout(state)
+
+  else:
+    echo("default state")
